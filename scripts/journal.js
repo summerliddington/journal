069f5a9bbff8c33c9eb3aa82b56c0abd6305d024
Summer Liddington <summerliddington@gmail.com>
2019-08-28 14:04:05 -0500

    first commit

---
+++ scripts/journal.js
@@ -0,0 +1,10 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntry = { 
+    entry: ["banana", "kiwi", "toast"]
+}
+
+journalEntry.entry.push("css");
+console.log(journalEntry.entry)
